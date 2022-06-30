53888c5a730069d7970f004469fc8354f29f38d6
AriRuo <ari.ruotsalainenn@gmail.com>
2022-06-30 11:34:38 +0300

    jeejejejejej

---
+++ index.js
@@ -0,0 +1,28 @@
+import {getUsers, getUserById, createUser, updateUser, deleteUser} from './queries.js' 
+import express from 'express'
+import bodyParser from 'body-parser'
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+  app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
+
+app.get('/users', getUsers)
+app.get('/users/:id', getUserById)
+app.post('/users', createUser)
+app.put('/users/:id', updateUser)
+app.delete('/users/:id', deleteUser)
+
+  
